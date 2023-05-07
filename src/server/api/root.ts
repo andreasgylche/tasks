2cf25a1cee007e54ec1cc5ff3984fb0a197a9439
Andreas Vifert <andreasgylche@gmail.com>
2023-05-07 18:58:37 +0200

    create task tRPC router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { tasksRouter } from "~/server/api/routers/tasks";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  tasks: tasksRouter,
 });
 
 // export type definition of API
