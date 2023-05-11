53ff5ed2f5a4205af19504c3146192ca66e2b243
Andreas Vifert <andreasgylche@gmail.com>
2023-05-11 19:01:14 +0200

    add Task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { api } from "~/utils/api";
+import { format } from "date-fns";
+import { Task } from "@prisma/client";
 
-export default function Task() {
-  return <div>Task</div>;
+export default function Task({ task }: { task: Task }) {
+  const ctx = api.useContext();
+
+  const { mutate: toggleStatus } = api.tasks.toggleStatus.useMutation({
+    onSuccess: () => {
+      void ctx.tasks.get.invalidate();
+    },
+  });
+
+  const { mutate: deleteTask } = api.tasks.delete.useMutation({
+    onSuccess: () => {
+      void ctx.tasks.get.invalidate();
+    },
+  });
+  return (
+    <div key={task.id} className="flex gap-2">
+      <span onClick={() => deleteTask({ id: task.id })}>❌</span>
+      <span onClick={() => toggleStatus({ isDone: !task.done, id: task.id })}>
+        ✅
+      </span>
+      <p>{task.task}</p>
+      <span>{task.done ? "Done" : "Not done"}</span>
+      <span>{format(task.createdAt, "dd MMMM yyyy")}</span>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,13 @@ import { api } from "~/utils/api";
 import { Spinner } from "~/components/ui/Spinner";
 import SubmitTask from "~/components/SubmitTask";
 import PageLayout from "~/components/ui/Layout";
+import Task from "~/components/Task";
+const { format } = require("date-fns");
 
 const Home: NextPage = () => {
   const { data, isLoading } = api.tasks.get.useQuery();
 
   console.log(data);
-  const ctx = api.useContext();
-
-  const { mutate: toggleStatus } = api.tasks.toggleStatus.useMutation({
-    onSuccess: () => {
-      void ctx.tasks.get.invalidate();
-    },
-  });
-
-  const { mutate: deleteTask } = api.tasks.delete.useMutation({
-    onSuccess: () => {
-      void ctx.tasks.get.invalidate();
-    },
-  });
 
   return (
     <>
@@ -35,17 +24,7 @@ const Home: NextPage = () => {
         <SubmitTask />
         {isLoading && <Spinner />}
         {data?.map((task) => (
-          <div key={task.id} className="flex gap-2">
-            <span onClick={() => deleteTask({ id: task.id })}>❌</span>
-            <span
-              onClick={() => toggleStatus({ isDone: !task.done, id: task.id })}
-            >
-              ✅
-            </span>
-            <p>{task.task}</p>
-            <span>{task.done ? "Done" : "Not done"}</span>
-            <span>{task.createdAt.toString()}</span>
-          </div>
+          <Task key={task.id} task={task} />
         ))}
       </PageLayout>
     </>
