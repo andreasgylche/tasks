1b098abaea362a1ffc5c0cd12fb1bccae03e9bd3
andreasgylche <andreasgylche@gmail.com>
2023-05-15 21:03:35 +0200

    add theme styles to welcome message

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Welcome() {
   return (
-    <div className="text-justify text-neutral-100/50">
-      <span className="font-bold text-neutral-100">
+    <div className="text-justify text-neutral-900/50 dark:text-neutral-100/50">
+      <span className="font-bold text-neutral-900 dark:text-neutral-100">
         Say goodbye to paper to-do lists and hello to a more organized life.
       </span>{' '}
       Keep all your tasks organized and easily accessible anytime, anywhere.
