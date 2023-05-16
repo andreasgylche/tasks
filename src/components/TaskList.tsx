518c1a57648e130d7fd87f4af3615f9da8c0df8b
andreasgylche <andreasgylche@gmail.com>
2023-05-16 19:27:56 +0200

    total tasks added

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -7,12 +7,27 @@ import SubmitTask from './SubmitTask'
 export default function TaskList() {
   const { data, isLoading } = api.tasks.get.useQuery()
 
+  const completedTasks = data?.filter((task) => task.done).length
+  const totalTasks = data?.length
+
+  if (!data)
+    return (
+      <div className="w-full">
+        <span>No tasks...</span>
+      </div>
+    )
+
   return (
     <div className="w-full">
       <SubmitTask />
       <div className="flex w-full flex-col items-center gap-2">
+        <div className="mb-2 flex w-full justify-between text-sm opacity-50">
+          <span>{totalTasks} total tasks</span>
+          <span>
+            {completedTasks} out of {totalTasks} done
+          </span>
+        </div>
         {isLoading && <BigSpinner />}
-
         {data?.map((task) => (
           <Task key={task.id} task={task} />
         ))}
